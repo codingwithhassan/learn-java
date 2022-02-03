866bcca850f49e816bf7893e3210082bc2aa5236
codingwithhassan <codingwithme.hassan@gmail.com>
2022-02-03 22:45:02 +0500

    post-lab-3

---
+++ 1st-Semester/Lab3/SalesTax.java
@@ -0,0 +1,20 @@
+public class SalesTax{
+    public static void main(String[] args){
+        double netIncome = 100, // purchase amount
+            stateSalesTaxPercent = 0.4, countrySalesTaxPercent = 0.2;
+        double grossIncome, // before tax
+            stateTax, countryTax, totalTax, totalSales;
+
+        stateTax = stateSalesTaxPercent * netIncome;
+        countryTax = countrySalesTaxPercent * netIncome;
+        totalTax = stateTax + countryTax;
+        totalSales = totalTax + netIncome;
+
+        System.out.println("Amount of Purchase: " + netIncome);
+        System.out.println("Sate Sales Tax: " + stateTax);
+        System.out.println("Country Sales tax: " + countryTax);
+        System.out.println("Total Sales tax: " + totalTax);
+        System.out.println("Total Sales: " + totalSales);
+
+    }
+}
