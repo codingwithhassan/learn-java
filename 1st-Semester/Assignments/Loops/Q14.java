62be0d9dd35c3cb0fafe68afb84e1f9333004552
codingwithhassan <codingwithme.hassan@gmail.com>
2022-03-29 02:15:48 +0500

    not solved yet: next 10 prime numbers

--- 1st-Semester/Assignments/Loops/Q14.java
+++ 1st-Semester/Assignments/Loops/Q14.java
@@ -9,50 +9,56 @@ public class Q14 {
 
         boolean isPrime = true;
 
-        int sqrt = (int)Math.sqrt(num);
+        int sqrt = (int) Math.sqrt(num);
 
         int i = 2;
-        while(i <= sqrt){
-            if(num % i == 0){
+        while (i <= sqrt) {
+            if (num % i == 0) {
                 isPrime = false;
             }
             i++;
         }
 
-        if(isPrime){
+        if (isPrime) {
             System.out.println(num + " is Prime Number");
             int p = num;
-            int count = 0,iteration;
+            int count = 0, iteration;
             boolean isPrimeNum = true;
-            while(count != 10){
-                    iteration = 2;
-                    while(iteration < (int)Math.sqrt(p)){
-                        if(p % iteration == 0)
-                            isPrimeNum = false;
-                        iteration++;
-                    }
-                    if(isPrimeNum){
-                        System.out.print(p + " ");
-                        count++;
-                    }
+            int sqrtNum;
+            while (count < 10) {
+                iteration = 2;
+                sqrtNum = (int) Math.sqrt(p);
+                while (iteration <= sqrtNum) {
+                    if (p % iteration == 0)
+                        isPrimeNum = false;
+                    iteration++;
+                }
+                if (isPrimeNum) {
+                    System.out.print(p + " ");
+                    count++;
+                }
+
                 p++;
             }
-        }else{
+        } else {
             System.out.println(num + " is not Prime Number");
             int p = num;
-            int count = 0,iteration;
+            int count = 0, iteration;
             boolean isPrimeNum = true;
-            while(count != 1){
-                    iteration = 2;
-                    while(iteration < (int)Math.sqrt(p)){
-                        if(p % iteration == 0)
-                            isPrimeNum = false;
-                        iteration++;
-                    }
-                    if(isPrimeNum){
-                        System.out.print(p + " ");
-                        count++;
-                    }
+            int sqrtNum;
+            while (count < 1) {
+                iteration = 2;
+                sqrtNum = (int) Math.sqrt(p);
+                while (iteration <= sqrtNum) {
+                    if (p % iteration == 0)
+                        isPrimeNum = false;
+                    iteration++;
+                }
+                if (isPrimeNum) {
+                    System.out.print(p + " ");
+                    count++;
+                }
+
                 p++;
             }
         }
