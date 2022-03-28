52f47feee49422b5a379b3aa04607bd47cee9b44
codingwithhassan <codingwithme.hassan@gmail.com>
2022-03-28 14:18:32 +0500

    decimal, octal, binary conversions

--- 1st-Semester/Assignments/Loops/Q6.java
+++ 1st-Semester/Assignments/Loops/Q6.java
@@ -6,17 +6,43 @@ public class Q6 {
 
 		Scanner in = new Scanner(System.in);
 
-		int dec;
+		int dec,hex,oct,bin,
+			rem;
+		String hexAns, octAns, binAns;
+		hexAns = octAns = binAns = "";
 
 		System.out.print("Enter Decimal Number: ");
 
 		dec = in.nextInt();
 
-		System.out.println("Hexa Number:   " + Integer.toHexString(dec));
+		hex = oct = bin = dec;
 
-		System.out.println("Octa Number:   " + Integer.toOctalString(dec));
+		while(hex >= 16){
+			rem = (int)hex%16;
+			hex /= 16;
+			hexAns = rem + hexAns;
+		}
+		hexAns = hex + hexAns;
 
-		System.out.println("Binay Number:   " + Integer.toBinaryString(dec));
+		while(oct >= 8){
+			rem = (int)oct%8;
+			oct /= 8;
+			octAns = rem + octAns;
+		}
+		octAns = oct + octAns;
+
+		while(bin >= 2){
+			rem = (int)bin%2;
+			bin /= 2;
+			binAns = rem + binAns;
+		}
+		binAns = bin + binAns;
+
+		System.out.println("Hexa Number:   " + hexAns);
+
+		System.out.println("Octa Number:   " + octAns);
+
+		System.out.println("Binay Number:   " + binAns);
 
 	}
 
