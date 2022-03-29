c401167d9e70ab63191d5490cc2558d78709dffd
codingwithhassan <codingwithme.hassan@gmail.com>
2022-03-29 12:49:58 +0500

    perfect number

--- 1st-Semester/Assignments/Loops/Q4/PerfectNumber.java
+++ 1st-Semester/Assignments/Loops/Q4/PerfectNumber.java
@@ -5,7 +5,26 @@ import java.util.Scanner;
 public class PerfectNumber {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = in.nextInt();
+        int sum = 0;
 
-        
+        System.out.print("Factors are: ");
+        int i = 1;
+        while(i < num){
+            if(num % i == 0){
+                sum += i;
+                System.out.print(i + " ");
+            }
+            i++;
+        }
+
+        System.out.println();
+
+        if(sum == num)
+            System.out.println("Perfect Number");
+        else
+            System.out.println("Not Perfect Number");
+
     }
 }
