d057246753476c2f407e930f4a710d9d0f25d80f
codingwithhassan <codingwithme.hassan@gmail.com>
2022-03-29 12:45:16 +0500

    frequency of digits

--- 1st-Semester/Assignments/Loops/Q4/FrequencyOfDigit.java
+++ 1st-Semester/Assignments/Loops/Q4/FrequencyOfDigit.java
@@ -5,14 +5,22 @@ import java.util.Scanner;
 public class FrequencyOfDigit {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int num,i,j,tmp,digit;
-
+        String num;
         System.out.print("Enter a number: ");
-        num = in.nextInt();
-        tmp = num;
-        while(tmp != 0){
-            
-        }
+        num = in.next();
+        char ch;
+        int i = 0,j,count;
+        while(i < num.length()){
+            ch = num.charAt(i);
+            count = j = 0;
+            while(j < num.length()){
+                if(ch == num.charAt(j))
+                    count++;
+                j++;
+            }
 
+            System.out.println(ch + " => " + count);
+            i++;
+        }
     }
 }
