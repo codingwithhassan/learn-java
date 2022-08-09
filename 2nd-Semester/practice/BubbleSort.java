5817cc9ba2e02f456a48134da43bc67846b8b5d3
codingwithhassan <codingwithme.hassan@gmail.com>
2022-08-09 15:47:50 +0500

    bubble sort & binary search

---
+++ 2nd-Semester/practice/BubbleSort.java
@@ -0,0 +1,39 @@
+public class BubbleSort {
+
+    static void bubbleSortForIntegers(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    // swap elements
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+    }
+
+    public static void bubbleSortForStrings() {
+
+        String str[] = { "Ajeet", "Steve", "Rick", "Becky", "Mohan" };
+        String temp;
+        System.out.println("Strings in sorted order:");
+
+        for (int j = 0; j < str.length; j++) {
+            for (int i = j + 1; i < str.length; i++) {
+                // comparing adjacent strings
+                if (str[i].compareTo(str[j]) < 0) {
+                    temp = str[j];
+                    str[j] = str[i];
+                    str[i] = temp;
+                }
+            }
+            System.out.println(str[j]);
+        }
+
+    }
+
+}
